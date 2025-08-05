4d6eaf3477cf2a7b86c499bc583b18be6dc45835
SyaPratama <rasyaputraq17@gmail.com>
2025-08-05 17:40:09 +0700

    fix(home): lint debug

--- src/contents/index/layout/ProjectContainer.tsx
+++ src/contents/index/layout/ProjectContainer.tsx
@@ -1,14 +1,15 @@
 import ProjectCard from "@/components/index/ProjectCard";
 import { projectData } from "@/data/dummy/project";
+import Image from "next/image";
 
 function ProjectContainer() {
   return (
     <div className="project-container w-fit h-screen flex gap-20 items-center flex-nowrap">
       {projectData.map((item, index) => (
         <ProjectCard key={index} className="bg-slate-900/80 backdrop-blur-2xl ring-4 ring-slate-600/50 w-[30rem] h-[20rem] md:w-[60rem] md:h-[35rem]">
-          <img
+          <Image
             className="absolute top-0 -z-10 w-full h-full rounded-2xl object-cover"
-            src={item.img}
+            src={item.img ?? ""}
             alt={item.title}
           />
           <div className="absolute w-full inset-0 rounded-2xl bg-gradient-to-t from-black/95 via-black/30 to-black/10 md:via-black/10"></div>
