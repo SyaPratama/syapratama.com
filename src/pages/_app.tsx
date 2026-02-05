47735eb54a7b94c922718fbda7e94878ee9cc812
SyaPratama <rasyaputraq17@gmail.com>
2026-02-05 10:27:50 +0700

    ads: add fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,28 +4,33 @@ import { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import getDefaultSeoConfig from "../../next-seo.config";
 import "@/styles/global.css";
+import Script from "next/script";
 
 function Root({ Component, pageProps }: AppProps) {
   const seoConfig = getDefaultSeoConfig();
-  return (
-    <html suppressHydrationWarning={true}>
-      <DefaultSeo {...seoConfig} />
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
 
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6200304491212280"
-          crossorigin="anonymous"></script>
-      </body>
-    </html>
+  return (
+    <>
+      <DefaultSeo {...seoConfig} />
+
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6200304491212280"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+    </>
   );
 }
 
